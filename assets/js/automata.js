726da7290424132e6fa21a057476dd63677bdfeb
Weslley de Campos <weslleydrum@hotmail.com>
2021-12-08 20:38:55 -0300

    creating node on canvas

--- assets/js/automata.js
+++ assets/js/automata.js
@@ -1,25 +1,62 @@
-var nodes = new vis.DataSet([
-  { id: 1, label: "Node 1" },
-  { id: 2, label: "Node 2" },
-  { id: 3, label: "Node 3" },
-  { id: 4, label: "Node 4" },
-  { id: 5, label: "Node 5" },
+
+let nodes = new vis.DataSet([
 ]);
 
 // create an array with edges
-var edges = new vis.DataSet([
-  { from: 1, to: 3 },
-  { from: 1, to: 2 },
-  { from: 2, to: 4 },
-  { from: 2, to: 5 },
-  { from: 3, to: 3 },
+let edges = new vis.DataSet([
 ]);
 
 // create a network
-var container = document.getElementById("mynetwork");
-var data = {
-  nodes: nodes,
-  edges: edges,
+let container = document.getElementById("mynetwork");
+let data = {
+  nodes,
+  edges
 };
 var options = {};
 var network = new vis.Network(container, data, options);
+
+const edgeFactory = (from, to) => {
+  return { from, to }
+}
+
+const nodeFactory = (id, label) => {
+  return {
+    id,
+    label
+  }
+}
+
+const addNode = () => {
+  const nodeId = document.getElementById('node-id').value;
+  const nodeLabel = document.getElementById('node-label').value
+
+  
+
+  const newNode = nodeFactory(Number(nodeId), nodeLabel);
+
+  nodes.add(newNode);
+
+}
+function updateNode() {
+
+}
+
+function removeNode() {
+
+}
+
+const addEdge = () => {
+  const edgeFrom = document.getElementById('edge-from').value
+  const edgeTo = document.getElementById('edge-to').value
+
+  const newEdge = edgeFactory(edgeFrom, edgeTo);
+  edges.add(newEdge)
+}
+
+function updateEdge() {
+
+}
+
+function removeEdge() {
+
+}
